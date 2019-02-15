dacec9493ecbc882c4c48208f718ae13d56e15f2
AntonBarbakov <barbak2017@gmail.com>
2019-02-16 00:36:10 +0200

    v3.0.2

--- js/script.js
+++ js/script.js
@@ -5,17 +5,24 @@ function showLeague(){
     for(let i=0;i<imgArr.length;i++){
         imgArr[i].setAttribute("src", "img/league/"+i+".jpg")
     }
+    $('#thirdMovieInformation').hide();
+    $('#secondMovieInformation').show();
+    $('#firstMovieInformation').hide();
 }
 
 function showAvengers(){
     for(let i=0;i<imgArr.length;i++){
         imgArr[i].setAttribute("src", "img/avengers/"+i+".jpg")
     }
-   
+    $('#thirdMovieInformation').hide();
+    $('#secondMovieInformation').hide();
+    $('#firstMovieInformation').show();
 }
 function showGuardians(){
     for(let i=0;i<imgArr.length;i++){
         imgArr[i].setAttribute("src", "img/guardians/"+i+".jpg")
     }
-   
+    $('#thirdMovieInformation').show();
+    $('#secondMovieInformation').hide();
+    $('#firstMovieInformation').hide();
 }
